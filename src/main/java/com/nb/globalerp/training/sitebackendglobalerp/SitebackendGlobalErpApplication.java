8c989a6407522ed4f1ea2a09a3d5ee82a59a0a43
e.davydov <davidoveg@gmail.com>
2026-04-04 01:32:08 +0700

    add group service

--- src/main/java/com/nb/globalerp/training/sitebackendglobalerp/SitebackendGlobalErpApplication.java
+++ src/main/java/com/nb/globalerp/training/sitebackendglobalerp/SitebackendGlobalErpApplication.java
@@ -2,12 +2,8 @@ package com.nb.globalerp.training.sitebackendglobalerp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync
-@EnableScheduling
 public class SitebackendGlobalErpApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/nb/globalerp/training/sitebackendglobalerp/services/GroupService.java
+++ src/main/java/com/nb/globalerp/training/sitebackendglobalerp/services/GroupService.java
@@ -4,32 +4,76 @@ import com.nb.globalerp.training.sitebackendglobalerp.api.dto.request.GroupPatch
 import com.nb.globalerp.training.sitebackendglobalerp.api.dto.request.GroupRequest;
 import com.nb.globalerp.training.sitebackendglobalerp.api.dto.response.GroupResponse;
 import com.nb.globalerp.training.sitebackendglobalerp.mapper.GroupMapper;
+import com.nb.globalerp.training.sitebackendglobalerp.persistence.entity.Course;
+import com.nb.globalerp.training.sitebackendglobalerp.persistence.entity.CourseCompletionStatus;
+import com.nb.globalerp.training.sitebackendglobalerp.persistence.entity.Group;
+import com.nb.globalerp.training.sitebackendglobalerp.persistence.entity.Specification;
+import com.nb.globalerp.training.sitebackendglobalerp.persistence.repo.CourseCompletionStatusRepository;
+import com.nb.globalerp.training.sitebackendglobalerp.persistence.repo.CourseRepository;
 import com.nb.globalerp.training.sitebackendglobalerp.persistence.repo.GroupRepository;
+import com.nb.globalerp.training.sitebackendglobalerp.persistence.repo.SpecificationRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class GroupService {
 
+    private final CourseCompletionStatusRepository courseCompletionStatusRepository;
+    private final CourseRepository courseRepository;
+    private final SpecificationRepository specificationRepository;
     private final GroupRepository groupRepository;
+
     private final GroupMapper groupMapper;
 
+    @Transactional
     public GroupResponse findById(int id) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        Group group = groupRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Group not found with id: " + id));
+
+        GroupResponse groupResponse = groupMapper.toGroupResponse(group);
+        groupResponse.setCourseCompletion(group.getCourseCompletionStatus().getName());
+
+        return groupResponse;
     }
 
+    @Transactional
     public int create(GroupRequest request) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        Course course = courseRepository.findById(request.courseId())
+            .orElseThrow(() -> new EntityNotFoundException("Group not found with id: " + request.courseId()));
+
+        CourseCompletionStatus courseCompletionStatus = courseCompletionStatusRepository.findById(request.courseCompletionId())
+            .orElseThrow(() -> new EntityNotFoundException("CourseCompletionId not found with id: " + request.courseCompletionId()));
+
+        Specification specification = specificationRepository.findById(request.specificationId())
+            .orElseThrow(() -> new EntityNotFoundException("Specification not found with id: " + request.specificationId()));
+
+        Group group = groupMapper.toGroupEntity(request);
+        group.setCourse(course);
+        group.setCourseCompletionStatus(courseCompletionStatus);
+        group.setSpecification(specification);
+
+        return groupRepository.save(group).getId();
     }
 
+    @Transactional
     public GroupResponse update(int id, GroupPatchRequest request) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        Group group = groupRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Group not found with id: " + id));
+
+        groupMapper.updateGroupMapper(group, request);
+
+        GroupResponse groupResponse = groupMapper.toGroupResponse(groupRepository.save(group));
+        groupResponse.setCourseCompletion(group.getCourseCompletionStatus().getName());
+
+        return groupResponse;
     }
 
     public void delete(int id) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        groupRepository.deleteById(id);
     }
 }
