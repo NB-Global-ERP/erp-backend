491aeec00aa4e5a233d1a0d72f1ef103bd3e96b8
e.davydov <davidoveg@gmail.com>
2026-04-04 22:14:56 +0700

    remove annota

--- src/main/java/com/nb/globalerp/training/sitebackendglobalerp/services/CertificateService.java
+++ src/main/java/com/nb/globalerp/training/sitebackendglobalerp/services/CertificateService.java
@@ -1,10 +1,6 @@
 package com.nb.globalerp.training.sitebackendglobalerp.services;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@
-@RequiredArgsConstructor
-@Service
 public class CertificateService {
+
+
 }
